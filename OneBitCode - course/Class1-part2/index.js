7f9dd2348038457d1d792b826d9d074f35ea3a58
Max8779 <maximilianooliveira8779gmail.ocom>
2026-06-05 06:57:01 -0300

    Operações matematicas

---
+++ OneBitCode - course/Class1-part2/index.js
@@ -0,0 +1,9 @@
+let value1 = parseInt(prompt("Put a number"));
+let value2 = parseInt(prompt("put anoher number"));
+
+const more = value1 + " + " + value2 + " = " + (value1 + value2);
+const less = value1 + " - " + value2 + " = " + (value1 - value2);
+const times = value1 + " x " + value2 + " = " + (value1 * value2);
+const division = value1 + " / " + value2 + " = " + (value1 / value2);
+
+alert("Results in the 4 mathematical operations\n\n" + more + "\n\n" + less + "\n\n" + times + "\n\n" + division); 
